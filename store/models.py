1a61a245cc842e76a81d2ffa0f7e446de515e34e
Priyanshasharma520 <er.21priyansha@gmail.com>
2021-01-09 17:24:49 +0530

    first commit store

---
+++ store/models.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from store import db,login_manager
+from flask_login import UserMixin
+
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+  
+class User(db.Model,UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    fname= db.Column(db.String(20),nullable=False)
+    lname= db.Column(db.String(20),nullable=True)
+    phone = db.Column(db.String(13),unique=True,nullable=True)
+    address_1 = db.Column(db.String(200),unique=False,nullable=True)
+    address_2 = db.Column(db.String(200),unique=False,nullable=True)
+    password = db.Column(db.String(60), nullable=False)
+    
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.fname}','{self.address_1}','{self.address_2}','{self.phone}')"
+
